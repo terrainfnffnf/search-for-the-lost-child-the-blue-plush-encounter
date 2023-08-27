f1d770243a2a7aa1b37f132cfa07a5005cb0dc01
terrainfnffnf <143310438+terrainfnffnf@users.noreply.github.com>
2023-08-27 15:57:54 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+// Print "Hello, World!" to the console
+console.log("Hello, World!");
+
+// Calculate the sum of two numbers
+function calculateSum 
+    
+
+
+// Call the function and print the result
+   
+
+
+// Create an array and iterate over its map
